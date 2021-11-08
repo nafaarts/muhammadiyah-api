b22564f0ca46aedcff09a679aa49ee161d186890
Nafaarts <71387948+nafaarts@users.noreply.github.com>
2021-11-08 11:26:30 +0700

    fix cors #7

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -18,6 +18,8 @@ class CorsMiddleware
         $headers = [
             'Access-Control-Allow-Origin'      => '*',
             'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Max-Age'           => '86400',
             'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
         ];
 
